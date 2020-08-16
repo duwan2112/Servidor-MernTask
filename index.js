3c996f3181c6e6f196f30125c54a9973a3de5622
Duwan Peña <duwanp2112@gmail.com>
2020-08-16 14:27:11 -0430

    Arreglando Proyecto

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const PORT = process.env.PORT || 3080
+const port = process.env.PORT || 3080
 const conectarDB = require('./config/db')
 
 //Conectando base de datos
@@ -16,6 +16,6 @@ app.use('/api/auth',require('./routes/AutenticadoRoutes'))
 app.use('/api/proyectos',require('./routes/ProyectosRoutes'))
 app.use('/api/tareas',require('./routes/TareasRoutes'))
 
-app.listen(PORT,()=>{
+app.listen(port,'0.0.0.0',()=>{
     console.log("servidor abierto en puerto 3080")
 })
